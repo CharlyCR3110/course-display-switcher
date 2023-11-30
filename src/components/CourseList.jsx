d347bb31b135a5cd4bec6d7a0582fb87fd2c8169
Charly <carlosgarita3110@gmail.com>
2023-11-30 17:44:38 -0600

    refactor: change CourseList component to include displayStyle prop

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,17 +1,20 @@
-import CourseCard from './courseCard'
+import RenderComponent from './RenderComponent'
 import '../styles/CourseList.css'
 
-const CourseList = ({ courses }) => {
+const CourseList = ({ courses, displayStyle }) => {
+  const sectionClassName = 'course-list ' + displayStyle.toLowerCase()
+
   return (
-    <section className='course-list'>
-      <h2 className='course-list-title'>Courses</h2>
+    <section className={sectionClassName}>
+      <h1 className='course-list-title'>Courses display in {displayStyle}</h1>
       {courses.map((course) => (
-        <CourseCard
+        <RenderComponent
           key={course.id}
           courseImage={course.image}
           courseName={course.name}
           courseArea={course.area}
           percentageCompleted={course.percentage}
+          displayStyle={displayStyle}
         />
       ))}
     </section>
