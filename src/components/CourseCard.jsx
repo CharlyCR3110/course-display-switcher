08a91a73ada953385dcc584c2d0c11785924ff0a
Charly <carlosgarita3110@gmail.com>
2023-11-30 16:58:55 -0600

    refactor: change CourseCard component to use props

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import '../styles/CourseCard.css';
+import React from 'react'
+import '../styles/CourseCard.css'
 
-const CourseCard = ( {courseImage, courseName, courseArea, percentageCompleted } ) => {
-	return (
-		<section className="card">
-			<img src="https://www.open.edu/openlearn/pluginfile.php/3277384/tool_ocwmanage/articletext/0/become_a_student_inline.jpg" alt="Course" className="card-image"/>
-			<h3 className="card-name">Curso de React</h3>
-			<small className="card-area">React</small>
-			<div className="card-progress-bar">
-				<div className="card-progress"></div>
-			</div>
-			
-		</section>
-	);
+const CourseCard = ({ courseImage, courseName, courseArea, percentageCompleted }) => {
+  const progressStyles = {
+    width: `${percentageCompleted}%`
+  }
+
+  return (
+    <section className='card'>
+      <img src={courseImage} alt='Course' className='card-image' />
+      <h3 className='card-name'>{courseName}</h3>
+      <small className='card-area'>{courseArea}</small>
+      <div className='card-progress-bar'>
+        <div className='card-progress' style={progressStyles} />
+      </div>
+
+    </section>
+  )
 }
 
-export default CourseCard;
+export default CourseCard
