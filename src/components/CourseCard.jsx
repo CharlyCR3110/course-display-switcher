139a2a187e4b5e5229f677c08a7650ab325f0f3f
Charly <carlosgarita3110@gmail.com>
2023-11-30 18:09:11 -0600

    refactor: change CourseCard component to use CourseBase component

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import '../styles/CourseCard.css'
+import CourseBase from './CourseBase'
+import '../styles/Common.css'
 
 const CourseCard = ({ courseImage, courseName, courseArea, percentageCompleted }) => {
-  const progressStyles = {
-    width: `${percentageCompleted}%`
-  }
-
   return (
-    <section className='card'>
-      <img src={courseImage} alt='Course' className='card-image' />
-      <h3 className='card-name'>{courseName}</h3>
-      <small className={`card-area ${courseArea.toLowerCase().replace(' ', '-')}`}>{courseArea}</small>
-      <div className='card-progress-bar'>
-        <div className='card-progress' style={progressStyles} />
-      </div>
-
-    </section>
+    <CourseBase
+      courseImage={courseImage}
+      courseName={courseName}
+      courseArea={courseArea}
+      percentageCompleted={percentageCompleted}
+      containerClass='card'
+      imageClass='card-image'
+      nameClass='card-name'
+      areaClass='card-area'
+      progressBarClass='card-progress-bar'
+      progressClass='card-progress'
+    />
   )
 }
 
